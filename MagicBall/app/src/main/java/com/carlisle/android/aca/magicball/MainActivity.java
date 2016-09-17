78fa10ddc1c048d3dfea84f19b9dcb01110d35ee
Chris Carlisle <ccarlis@gmail.com>
2016-09-16 22:07:03 -0500

    Committed unsaved work.

---
+++ MagicBall/app/src/main/java/com/carlisle/android/aca/magicball/MainActivity.java
@@ -0,0 +1,86 @@
+package com.carlisle.android.aca.magicball;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final TextView answerText = (TextView) findViewById(R.id.textView);
+
+        String[] anArray = new String[20];
+        anArray[0] = "Yep";
+        anArray[1] = "You bet";
+        anArray[2] = "uh huh";
+        anArray[4] = "durn tootin";
+        anArray[5] = "I reckon";
+        anArray[6] = "Sho nuff";
+        anArray[7] = "Fo sho";
+        anArray[8] = "count on it";
+        anArray[9] = "Most likely";
+        anArray[10] = "dunno";
+        anArray[11] = "huh?";
+        anArray[12] = "come again?;";
+        anArray[13] = "I can't understand your accent";
+        anArray[14] = "What kind of question is that?";
+        anArray[15] = "nope";
+        anArray[16] = "fat chance";
+        anArray[17] = "Come on, man!";
+        anArray[18] = "heck nah";
+        anArray[19] = "Don't count on it";
+
+        for (String s : anArray){
+            Log.i("" , s);
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randGenerator = new Random();
+                int ourRandomNumber = randGenerator.nextInt(20);
+
+                TextView.setText("", + ourRandomNumber);
+
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ NoteToSelf/app/src/main/java/com/carlisle/android/aca/notetoself/MainActivity.java
@@ -0,0 +1,177 @@
+package com.carlisle.android.aca.notetoself;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoteAdapter mNoteAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNoteAdapter = new NoteAdapter();
+
+        ListView listNote = (ListView) findViewById(R.id.listView);
+
+        listNote.setAdapter(mNoteAdapter);
+
+        // Handle clicks on the ListView
+        listNote.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int whichItem, long id) {
+
+    /*
+      Create  a temporary Note
+      Which is a reference to the Note
+      that has just been clicked
+    */
+                Note tempNote = mNoteAdapter.getItem(whichItem);
+
+                // Create a new dialog window
+                DialogShowNote dialog = new DialogShowNote();
+                // Send in a reference to the note to be shown
+                dialog.sendNoteSelected(tempNote);
+
+                // Show the dialog window with the note in it
+                dialog.show(getFragmentManager(), "");
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_add) {
+            DialogNewNote dialog = new DialogNewNote();
+            dialog.show(getFragmentManager(), "");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void createNewNote(Note n){
+
+        mNoteAdapter.addNote(n);
+    }
+
+    public class NoteAdapter extends BaseAdapter {
+
+        List<Note> noteList = new ArrayList<Note>();
+
+        @Override
+        public int getCount() {
+            return noteList.size();
+        }
+
+        @Override
+        public Note getItem(int whichItem) {
+            return noteList.get(whichItem);
+        }
+
+        @Override
+        public long getItemId(int whichItem) {
+            return whichItem;
+        }
+
+        @Override
+        public View getView(int whichItem, View view, ViewGroup viewGroup)   {
+
+            // Implement this method next
+
+            // Has view been inflated already
+            if(view == null){
+
+                // If not, do so here
+                // First create a LayoutInflater
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                // Now instantiate view using inflater.inflate
+                // using the listitem layout
+                view = inflater.inflate(R.layout.listitem, viewGroup,false);
+                // The false parameter is neccessary
+                // because of the way that we want to use listitem
+
+            }// End if
+
+            // Grab a reference to all our TextView and ImageView widgets
+            TextView txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+            TextView txtDescription = (TextView) view.findViewById(R.id.txtDescription);
+            ImageView ivImportant = (ImageView) view.findViewById(R.id.imageViewImportant);
+            ImageView ivTodo = (ImageView) view.findViewById(R.id.imageViewTodo);
+            ImageView ivIdea = (ImageView) view.findViewById(R.id.imageViewData);
+
+            // Hide any ImageView widgets that are not relevant
+            Note tempNote = noteList.get(whichItem);
+
+            if (!tempNote.isImportant()){
+                ivImportant.setVisibility(View.GONE);
+            }
+
+            if (!tempNote.isTodo()){
+                ivTodo.setVisibility(View.GONE);
+            }
+
+            if (!tempNote.isIdea()){
+                ivIdea.setVisibility(View.GONE);
+            }
+
+            // Add the text to the heading and description
+            txtTitle.setText(tempNote.getTitle());
+            txtDescription.setText(tempNote.getDescription());
+
+
+            return view;
+        }
+
+        public void addNote(Note n){
+            noteList.add(n);
+            notifyDataSetChanged();
+
+        }
+
+    }
+    }
+
+
