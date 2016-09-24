3803386b295d88132cbfbc14e17d6f8d8c1ea84f
Chris Carlisle <ccarlis@gmail.com>
2016-09-24 10:21:01 -0500

    Friday 9/23 work

---
+++ DualFragment/app/src/main/java/com/carlisle/android/aca/dualfragment/ActivityComs.java
@@ -0,0 +1,10 @@
+package com.carlisle.android.aca.dualfragment;
+
+/**
+ * Created by chriscarlisle on 9/23/16.
+ */
+
+public interface ActivityComs {
+    void onListItemSelected(int pos);}
+
+
---
+++ DualFragment/app/src/main/java/com/carlisle/android/aca/dualfragment/NameAndAddress.java
@@ -0,0 +1,45 @@
+package com.carlisle.android.aca.dualfragment;
+
+import java.io.Serializable;
+
+/**
+ * Created by chriscarlisle on 9/23/16.
+ */
+
+public class NameAndAddress implements Serializable {
+
+    private String mName;
+    private String mAddress1;
+    private String mAddress2;
+    private String mZipCode;
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getAddress1() {
+        return mAddress1;
+    }
+
+    public String getAddress2() {
+        return mAddress2;
+    }
+
+    public String getZipCode() {
+        return mZipCode;
+    }
+
+    public NameAndAddress(String name,
+                          String address1,
+                          String address2,
+                          String zipCode){
+
+        mName = name;
+        mAddress1 = address1;
+        mAddress2 = address2;
+        mZipCode = zipCode;
+
+    }
+}
+
+
