76a0ebfb1e2ca0a79f2dd4ec7273e34dd9012432
Chris Carlisle <ccarlis@gmail.com>
2016-10-06 13:14:29 -0500

    Committed Pong updates

--- Pong/app/src/main/java/com/carlisle/android/aca/pong/TapToPlay.java
+++ Pong/app/src/main/java/com/carlisle/android/aca/pong/TapToPlay.java
@@ -1,12 +1,12 @@
 package com.carlisle.android.aca.pong;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class TapToPlay extends AppCompatActivity {
+public class TapToPlay extends Activity {
     Button mButton;
 
     @Override
