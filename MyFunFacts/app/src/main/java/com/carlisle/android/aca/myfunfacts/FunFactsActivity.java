53bf955e8a03d0ab91b5464ebab3e6fe22896fe5
Chris Carlisle <ccarlis@gmail.com>
2017-05-23 13:08:35 -0500

    Update changes

--- MyFunFacts/app/src/main/java/com/carlisle/android/aca/myfunfacts/FunFactsActivity.java
+++ MyFunFacts/app/src/main/java/com/carlisle/android/aca/myfunfacts/FunFactsActivity.java
@@ -1,34 +1,47 @@
 package com.carlisle.android.aca.myfunfacts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
-
+    public static final String TAG =FunFactsActivity.class.getSimpleName();
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
     private TextView mFactTextView;
-    private Button mShowFactButtonm;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
 
-        mFactTextView = (TextView) findViewById(R.id.factTextView);
-        mShowFactButtonm = (Button)findViewById(R.id.showFactButton);
+        mFactTextView = (TextView)findViewById(R.id.factTextView);
+        mShowFactButton = (Button)findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
+
+                //update the screen with our dynamic fact
+                mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
 
             }
         };
-        mShowFactButtonm.setOnClickListener(listener);
+        mShowFactButton.setOnClickListener(listener);
 
-        string fact = "Ostriches can run faster than horses.";
-        mFactTextView.setText(fact);
+       // Toast.makeText(FunFactsActivity.this, "YAY! Our Activity Was Created", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We are logging from the onCreate method");
 
     }
 }
